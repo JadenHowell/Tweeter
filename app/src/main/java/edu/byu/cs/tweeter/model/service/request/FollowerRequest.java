38f7abe09466f2e25e6f9d8430e39e06124dcd1a
JadenHowell <speedingj@gmail.com>
2021-02-06 14:09:07 -0700

    Add follower fragment and all tasks/services needed for that

---
+++ app/src/main/java/edu/byu/cs/tweeter/model/service/request/FollowerRequest.java
@@ -0,0 +1,55 @@
+package edu.byu.cs.tweeter.model.service.request;
+
+/**
+ * Contains all the information needed to make a request to have the server return the next page of
+ * followers for a specified follower.
+ */
+public class FollowerRequest {
+    private final String followeeAlias;
+    private final int limit;
+    private final String lastFollowerAlias;
+
+    /**
+     * Creates an instance.
+     *
+     * @param followeeAlias the alias of the user whose followers are to be returned.
+     * @param limit the maximum number of followers to return.
+     * @param lastFollowerAlias the alias of the last follower that was returned in the previous request (null if
+     *                     there was no previous request or if no followers were returned in the
+     *                     previous request).
+     */
+    public FollowerRequest(String followeeAlias, int limit, String lastFollowerAlias) {
+        this.followeeAlias = followeeAlias;
+        this.limit = limit;
+        this.lastFollowerAlias = lastFollowerAlias;
+    }
+
+    /**
+     * Returns the followee whose followers are to be returned by this request.
+     *
+     * @return the follower.
+     */
+    public String getFolloweeAlias() {
+        return followeeAlias;
+    }
+
+    /**
+     * Returns the number representing the maximum number of followers to be returned by this request.
+     *
+     * @return the limit.
+     */
+    public int getLimit() {
+        return limit;
+    }
+
+    /**
+     * Returns the last follower that was returned in the previous request or null if there was no
+     * previous request or if no followers were returned in the previous request.
+     *
+     * @return the last follower.
+     */
+    public String getLastFollowerAlias() {
+        return lastFollowerAlias;
+    }
+
+}
