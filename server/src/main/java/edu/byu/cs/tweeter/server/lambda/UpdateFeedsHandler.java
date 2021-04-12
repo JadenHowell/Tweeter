dcc336e8f3e1f5d64b1350eebac05afe3f2381a2
JadenHowell <speedingj@gmail.com>
2021-04-11 21:06:21 -0600

    paginate feed and story retrieval, finish queues to update feeds

--- server/src/main/java/edu/byu/cs/tweeter/server/lambda/UpdateFeedsHandler.java
+++ server/src/main/java/edu/byu/cs/tweeter/server/lambda/UpdateFeedsHandler.java
@@ -11,23 +11,31 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import edu.byu.cs.tweeter.server.dao.FeedDAO;
+
 public class UpdateFeedsHandler implements RequestHandler<SQSEvent, Void> {
     public static String POSTER_ATTR = "poster";
     public static String TIMESTAMP_ATTR = "timestamp";
     public static String FOLLOWERS_ATTR = "followers";
     @Override
     public Void handleRequest(SQSEvent event, Context context) {
+        FeedDAO feedDAO = getFeedDAO();
         for (SQSEvent.SQSMessage msg : event.getRecords()) {
             String message = msg.getBody();
             Map<String, SQSEvent.MessageAttribute> attributeMap = msg.getMessageAttributes();
             String alias = attributeMap.get(POSTER_ATTR).getStringValue();
-            long timestamp = Long.parseLong(attributeMap.get(TIMESTAMP_ATTR).getStringValue());
+            String timestamp = attributeMap.get(TIMESTAMP_ATTR).getStringValue();
             String stringFollowers = attributeMap.get(FOLLOWERS_ATTR).getStringValue();
             Type listType = new TypeToken<ArrayList<String>>() {}.getType();
             List<String> followers = (new Gson()).fromJson(stringFollowers, listType);
-            System.out.println("alias: " + alias + "\nmessage: " + message + "\ntimestamp: " + timestamp + "\nfollowers: "+ followers);
-
+            for(int i = 0; i < followers.size(); i ++){
+                feedDAO.postToFeed(followers.get(i), alias, message, timestamp);
+            }
         }
         return null;
+    }
+
+    private FeedDAO getFeedDAO(){
+        return new FeedDAO();
     }
 }
